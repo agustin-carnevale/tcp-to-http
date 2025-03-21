3466e6df080c5e58926d45623cf55db5d16c65ff
Agustin Carnevale <agustinc.dev@gmail.com>
2025-03-21 00:05:31 -0300

    listen to tcp connections and read/print lines

--- main.go
+++ main.go
@@ -3,28 +3,39 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net"
 	"strings"
 )
 
 func main() {
+	port := "42069"
+	addr := ":" + port
 
-	file, err := os.Open("messages.txt")
+	tcpListener, err := net.Listen("tcp", addr)
 	if err != nil {
-		fmt.Println("Error opening file: messages.txt")
+		fmt.Println("Error creating TCP listener: ", err)
 	}
-	defer file.Close()
+	defer tcpListener.Close()
 
-	for line := range getLinesChannel(file) {
-		fmt.Printf("read: %s\n", line)
+	for {
+		conn, err := tcpListener.Accept()
+		if err != nil {
+			fmt.Println("Error while accepting next tcp connection:", err)
+		}
+		fmt.Println("New TCP connection:")
+
+		for line := range getLinesFromTCPChannel(conn) {
+			fmt.Printf("%s\n", line)
+		}
+		fmt.Println("Connection closed.")
+
 	}
-
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
+func getLinesFromTCPChannel(conn net.Conn) <-chan string {
 	ch := make(chan string)
 
-	// Go routine to read lines
+	// Go routine to read lines from TCP connn
 	go func() {
 		defer close(ch)
 
@@ -32,7 +43,7 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 		line := ""
 
 		for {
-			n, err := f.Read(buffer)
+			n, err := conn.Read(buffer)
 			if err != nil {
 				if err == io.EOF {
 					//End of file
@@ -44,24 +55,17 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 				fmt.Println("Error while reading file:", err)
 			}
 
-			bufferString := string(buffer[:n]) // Read only the n bytes read
+			bufferString := string(buffer[:n]) // Get only the n bytes that were read
 			parts := strings.Split(bufferString, "\n")
 
-			if len(parts) > 1 {
-				line += parts[0]
-				ch <- line      // Send the completed line
-				line = parts[1] // Start new line with remaining part
-			} else {
-				line += bufferString
+			// Read all parts that are complete lines
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- fmt.Sprintf("%s%s", line, parts[i]) // Send the completed line
+				line = ""
 			}
-
-			// other option
-			// for i := 0; i < len(parts)-1; i++ {
-			// 	ch <- fmt.Sprintf("%s%s", line, parts[i])
-			// 	line = ""
-			// }
-			// line += parts[len(parts)-1]
+			line += parts[len(parts)-1] // Start new line with remaining (last) part
 		}
 	}()
+
 	return ch
 }
---
+++ read_file.go
@@ -0,0 +1,67 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"io"
+// 	"os"
+// 	"strings"
+// )
+
+// func mainOld() {
+
+// 	file, err := os.Open("messages.txt")
+// 	if err != nil {
+// 		fmt.Println("Error opening file: messages.txt")
+// 	}
+// 	defer file.Close()
+
+// 	for line := range getLinesFromFileChannel(file) {
+// 		fmt.Printf("read: %s\n", line)
+// 	}
+
+// }
+
+// func getLinesFromFileChannel(f io.ReadCloser) <-chan string {
+// 	ch := make(chan string)
+
+// 	// Go routine to read lines
+// 	go func() {
+// 		defer close(ch)
+
+// 		buffer := make([]byte, 8)
+// 		line := ""
+
+// 		for {
+// 			n, err := f.Read(buffer)
+// 			if err != nil {
+// 				if err == io.EOF {
+// 					//End of file
+// 					if len(line) > 0 {
+// 						ch <- line
+// 					}
+// 					break
+// 				}
+// 				fmt.Println("Error while reading file:", err)
+// 			}
+
+// 			bufferString := string(buffer[:n]) // Read only the n bytes read
+// 			parts := strings.Split(bufferString, "\n")
+
+// 			if len(parts) > 1 {
+// 				line += parts[0]
+// 				ch <- line      // Send the completed line
+// 				line = parts[1] // Start new line with remaining part
+// 			} else {
+// 				line += bufferString
+// 			}
+
+// 			// other option
+// 			// for i := 0; i < len(parts)-1; i++ {
+// 			// 	ch <- fmt.Sprintf("%s%s", line, parts[i])
+// 			// 	line = ""
+// 			// }
+// 			// line += parts[len(parts)-1]
+// 		}
+// 	}()
+// 	return ch
+// }

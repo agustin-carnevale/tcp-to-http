44da8bef0b96cdb565bbe4945b0a3305b77a917a
Agustin Carnevale <agustinc.dev@gmail.com>
2025-03-22 01:04:31 -0300

    reading and parsing from reader in chunks of bytes

--- internal/request/request.go
+++ internal/request/request.go
@@ -8,6 +8,7 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+	state       requestState
 }
 
 type RequestLine struct {
@@ -16,51 +17,149 @@ type RequestLine struct {
 	Method        string
 }
 
+type requestState int
+
 const (
-	CRLF = "\r\n"
+	REQUEST_INITIALIZED requestState = iota
+	REQUEST_COMPLETED
 )
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	// buffer := make([]byte, 8, 8)
+const CRLF = "\r\n"
+const INITIAL_BUFFER_SIZE = 8
 
-	entireReqBytes, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	buffer := make([]byte, INITIAL_BUFFER_SIZE)
+
+	readToIndex := 0
+	request := Request{
+		state: REQUEST_INITIALIZED,
 	}
 
-	entireReqString := string(entireReqBytes)
+	for request.state != REQUEST_COMPLETED {
+		if readToIndex >= len(buffer) {
+			// if buffer is full duplicate size/capacity
+			newBuffer := make([]byte, len(buffer)*2)
+			copy(newBuffer, buffer)
+			buffer = newBuffer
+		}
 
+		// READ INTO BUFFER
+		numBytesRead, err := reader.Read(buffer[readToIndex:])
+		if err != nil {
+			if err == io.EOF {
+				request.state = REQUEST_COMPLETED
+				break
+			}
+			return nil, err
+		}
+
+		// update/advance "pointer" after reading n bytes
+		readToIndex += numBytesRead
+
+		// PARSE FROM THE BUFFER
+		numBytesParsed, err := request.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		// Shift remaining unparsed data to the beginning of the buffer
+		copy(buffer, buffer[numBytesParsed:])
+		readToIndex -= numBytesParsed
+
+	}
+	return &request, nil
+}
+
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	entireReqString := string(data)
 	requestParts := strings.Split(entireReqString, CRLF)
 
-	if len(requestParts) == 0 {
-		return nil, errors.New("invalid request format")
+	// Validate parts divided by CRLF
+	if len(requestParts) < 2 {
+		// means not enough data to read entire request-line
+		return nil, 0, nil
 	}
 
-	requestLine := requestParts[0]
+	// First part should be a RequestLine
+	requestLineString := requestParts[0]
 
-	requestLineParts := strings.Split(requestLine, " ")
+	requestLine, err := parseRequestLineFromString(requestLineString)
+	if err != nil {
+		return nil, 0, err
+	}
 
+	// if parse was successful then I parsed the whole requestLine string + the CRLF (which is ignored)
+	bytesParsed := len(requestLineString) + len(CRLF)
+
+	return requestLine, bytesParsed, nil
+}
+
+func parseRequestLineFromString(requestLineString string) (*RequestLine, error) {
+	requestLineParts := strings.Split(requestLineString, " ")
+
+	// Validate RequestLine parts
 	if len(requestLineParts) != 3 {
 		return nil, errors.New("invalid request format")
 	}
 
-	// Validate fields
+	// RequestLine fields
 	method := requestLineParts[0]
 	target := requestLineParts[1]
-	httpVersion := requestLineParts[2]
+	version := requestLineParts[2]
 
-	if httpVersion != "HTTP/1.1" ||
-		!isValidHTTPMethod(method) ||
-		!strings.HasPrefix(target, "/") {
-		return nil, errors.New("invalid request format")
+	//Validate version
+	versionParts := strings.Split(version, "/")
+	if len(versionParts) != 2 {
+		return nil, errors.New("invalid request http version format")
+	}
+	httpVersion := versionParts[1]
+	if httpVersion != "1.1" {
+		return nil, errors.New("invalid request version")
 	}
 
-	request := Request{
-		RequestLine: RequestLine{
-			HttpVersion:   "1.1",
-			RequestTarget: target,
-			Method:        method,
-		},
+	//Validate method
+	if !isValidHTTPMethod(method) {
+		return nil, errors.New("invalid request method")
 	}
-	return &request, nil
+
+	//Validate target
+	if !strings.HasPrefix(target, "/") {
+		return nil, errors.New("invalid request target")
+	}
+
+	requestLine := RequestLine{
+		HttpVersion:   httpVersion,
+		RequestTarget: target,
+		Method:        method,
+	}
+
+	return &requestLine, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+
+	// if request already completed
+	if r.state == REQUEST_COMPLETED {
+		return 0, errors.New("error: trying to read data in a done state")
+	}
+
+	// if request is not completed
+	if r.state == REQUEST_INITIALIZED {
+		requestLine, numBytesParsed, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if numBytesParsed == 0 {
+			// couldn't parse yet, more data needed
+			return 0, nil
+		}
+
+		// if bytes consumed then update requestLine and state
+		r.RequestLine = *requestLine
+		r.state = REQUEST_COMPLETED
+
+		return numBytesParsed, nil
+	}
+
+	return 0, errors.New("error: unknown parser state")
 }
